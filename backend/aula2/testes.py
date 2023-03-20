bfedf45511f54ddf337c6b3f056313880109c846
gabrielgusn <gabriel.gnicolodi@gmail.com>
2023-03-20 19:21:56 -0300

    chore: adicionados arquivos da aula 2 de backend - 🔧

---
+++ backend/aula2/testes.py
@@ -0,0 +1,13 @@
+nota_1 = 8.5
+nota_2 = 7
+nome = 'Emanoeli'
+
+print(nota_1)
+print("adress:", id(nota_1))
+print("size:", nota_1.__sizeof__())
+print(nota_2)
+print("adress:", id(nota_2))
+print("size:", nota_2.__sizeof__())
+print(nome)
+print("adress:", id(nome))
+print("size:", nome.__sizeof__())
